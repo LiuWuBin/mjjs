a5f9f02c17926960e76db0299af28827f79e7848
LiuWuBin <1393661433@qq.com>
2018-09-29 17:47:14 +0800

    20180929  add
    index页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,19 @@ const app = getApp()
 Page({
   data: {
     sportList:[1,2,3],
-    
+    commentaryTypeArray: [
+        {
+            "type": "人气",
+            "isSelect": true
+        },
+        {
+            "type": "命中率",
+            "isSelect": false
+        },
+        {
+            "type": "带红人数",
+            "isSelect": false
+        }
+    ]
   }
 })
